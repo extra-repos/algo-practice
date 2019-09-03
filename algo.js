f51226aecdb47438c12a1f014debf339a0796bc7
Jonathan Biro <jonnybrx@gmail.com>
2019-09-03 15:46:07 -0400

    Update algo.js

--- algo.js
+++ algo.js
@@ -29,4 +29,22 @@ function factorial (num){
 }
 factorial(6)
 
+//fib - Returns the nth Fibonacci number.
+
+fib(0)                              // 0
+fib(1)                              // 1
+fib(10)                             // 55
+fib(20)                             // 6765
+
+function fib (num){
+  if (num === 0){
+    return 0
+  } else if(num === 1){
+    return 1
+  } else {
+    return fib(num-1) + fib(num-2)
+  }
+}
+fib(10)
+
 //
